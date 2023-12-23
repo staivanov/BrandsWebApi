b3a38c1e4b6a8f3d30de99db6f0d3f579f5c3c44
Stanislav Ivanov <40460292+staivanov@users.noreply.github.com>
2023-12-23 23:56:21 +0200

    Date seed.

--- CarBrands.WebApi.Context/CarBrandsContext.cs
+++ CarBrands.WebApi.Context/CarBrandsContext.cs
@@ -65,7 +65,34 @@ namespace CarBrands.WebApi.Context
                     DateCreated = new DateOnly(1937, 5, 28),
                     HeadquarterId = 4
                 },
-
+                new()
+                {
+                    Id = 5,
+                    Name = "Opel",
+                    Description = @"Opel AG, German automotive company, a wholly owned subsidiary of Stellantis NV, specializing in the manufacture of passenger
+                        cars and light vans. ",
+                    DateCreated = new DateOnly(1862, 1, 21),
+                    HeadquarterId = 5
+                },
+                new()
+                {
+                    Id = 6,
+                    Name = "Audi",
+                    Description = @"Audi AG (Audi) a subsidiary of Volkswagen AG, is an automobile manufacturer. It designs, develops, 
+                        manufactures, and commercializes premium cars, and motorcycles globally.",
+                    DateCreated = new DateOnly(1909, 7,16),
+                    HeadquarterId = 6
+                },
+                new ()
+                {
+                    Id = 7,
+                    Name = "Toyota",
+                    Description = @"Toyota Motor Corp (Toyota) is an automobile manufacturer. The company designs, manufactures and sells passenger cars, 
+                        buses, minivans, trucks, specialty cars, recreational and sport-utility vehicles.",
+                    DateCreated = new DateOnly(1937, 8, 28),
+                    HeadquarterId = 7
+                }
+                    
             };
 
             List<CarModel> carModels = new()
@@ -111,6 +138,22 @@ namespace CarBrands.WebApi.Context
                 " employees are working in various central functions and services and give " +
                 "the impetus for our way to a successful future.", "Mercedesstraße 120\r\n70372 Stuttgart-Untertürkheim", new DateOnly(1926, 1,1)),
                 new(4, "Volkswagen Group Tower", "No information is provided.", "Berliner Ring 2, 38440 Wolfsburg", new DateOnly(1938,1,1)),
+                new(5, "Opel Russelsheim Facility", "No information is provided.", "65423 Rüsselsheim am Main", new DateOnly(1945, 1,1)),
+                new(6, "Audi Headquarter", "The plant in the center of Bavaria is not only the production site of AUDI AG, but also home to Group headquarters and Technical Development." +
+                " The Audi A3, Audi A4 and Audi A5 model series as well as the Audi Q2 are produced here. In 2022, 332,981 cars will have rolled off the" +
+                " production line at the Ingolstadt site. With 40,118 employees, Audi is the largest employer in the region. The plant is preparing " +
+                "for the production start-up of the first all-electric model, Audi Q6 e-tron. A dedicated battery assembly line is currently being" +
+                " built for this purpose in front of the factory gates. Thanks to extensive flexibilization of the production lines," +
+                " the site is ideally prepared for the gradual switch to electromobility and the production of further all-electric models.",
+                "AUDI AG Auto-Union-Str. 1 85057 Ingolstadt ", new DateOnly(1949,1,1)),
+                new(7, "Toyota Motor Corporation HQ", "Headquarter of one of the biggest in the world comapnies for cars.",
+                "1 Toyota-Cho, Toyota City, Aichi Prefecture 471-8571", new DateOnly(1976, 1,1)),
+                new(8, "Honda Motor Co. Tower", "Honda main HQ is Minato. The city is a special ward in the Tokyo Metropolis in Japan." +
+                " It is also called Minato City in English. It was formed in 1947 as a merger of Akasaka," +
+                " Azabu and Shiba wards following Tokyo City's transformation into Tokyo Metropolis", "2-1-1, Minami-Aoyama, Minato-ku", new DateOnly(1978, 1,1)),
+                new(9, "Lexus USA Headquarter", "No information is provided.", "6565 Headquarters Dr, Plano, TX 75024", new DateOnly(1989, 1,1)),
+                new(10, "Mazda Japan Co Headquarter", "The head office opened March 1931 and continues to serve as a site for piston engine and transmission production." +
+                "The facility is 55.1 hectares (136 acres) in size.", "59 Grigore Alexandrescu, HQ Victoriei Building", new DateOnly(1931, 3,10))
             };
 
             List<CountryOfProduction> countriesOfProduction = new()
